235c89c66bb9b7b21c3ed51f84579b34bceb66bc
kunaltyagi26 <kunaltyagi32@gmail.com>
2020-01-18 12:05:17 +0530

    First commit

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ToDoListViewController.swift
+//  Todoey
+//
+//  Created by Kunal Tyagi on 18/01/20.
+//  Copyright © 2020 Kunal Tyagi. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    fileprivate let items = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell") else { return UITableViewCell() }
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+
+}
